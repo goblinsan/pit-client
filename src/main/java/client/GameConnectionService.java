0be8605a6d8a31b46e4c449e0904b56d279b90b0
goblinsan <coghlanjames@yahoo.com>
2018-06-02 17:15:35 -0400

    add connection info into connection service

--- src/main/java/client/GameConnectionService.java
+++ src/main/java/client/GameConnectionService.java
@@ -1,18 +1,75 @@
 package client;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
 class GameConnectionService {
-    boolean joinGame() {
-        return true;
+
+    private final String baseHost;
+    private final String basePort;
+    private final String username;
+    private final String password;
+    RestTemplate restTemplate = new RestTemplate();
+    private final HttpHeaders headers = new HttpHeaders();
+
+    GameConnectionService(String baseHost, String basePort, String username, String password) {
+        this.baseHost = baseHost;
+        this.basePort = basePort;
+        this.username = username;
+        this.password = password;
+
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        MappingJackson2HttpMessageConverter jsonMessageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverters.add(jsonMessageConverter);
+        restTemplate.setMessageConverters(messageConverters);
+
+        String plainCreds = username+":"+password;
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        headers.add("Authorization", "Basic " + base64Creds);
+    }
+
+    boolean schedule(String state) {
+        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(baseHost).port(basePort)
+                .path("/admin/schedule/{state}").buildAndExpand(state);
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, request, String.class);
+        return response.getStatusCode().is2xxSuccessful() && response.getBody().equalsIgnoreCase("SCHEDULED");
+    }
+
+    boolean connect() {
+        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(baseHost).port(basePort)
+                .path("/connect/{name}").buildAndExpand(username);
+        ResponseEntity<String> response = restTemplate.getForEntity(uriComponents.toUriString(),String.class);
+        return response.getStatusCode().is2xxSuccessful() && response.getBody().equalsIgnoreCase("CONNECTED");
     }
 
     Map<String, Integer> getHand() {
-        return null;
+        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(baseHost).port(basePort)
+                .path("/player/hand/{name}").buildAndExpand(username);
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<Map> response = restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, request, Map.class);
+        return response.getBody();
     }
 
     List<Offer> getOffers() {
+        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(baseHost).port(basePort)
+                .path("/player/hand/{name}").buildAndExpand(username);
+
         return null;
     }
 
--- src/test/java/client/GameConnectionServiceTest.java
+++ src/test/java/client/GameConnectionServiceTest.java
@@ -2,14 +2,30 @@ package client;
 
 import org.junit.Test;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GameConnectionServiceTest {
-    private GameConnectionService testObject = new GameConnectionService();
+    private GameConnectionService testObject = new GameConnectionService("localhost", "8080", "LUKE", "password");
+
+    @Test
+    public void startGameConnectPlayerOpenMarket() {
+        GameConnectionService adminService = new GameConnectionService("localhost", "8080", "admin", "chase123");
+        assertTrue(adminService.schedule("start"));
+        assertTrue(testObject.connect());
+        assertTrue(adminService.schedule("open"));
+    }
 
     @Test
     public void clientCanJoinGame() {
-        assertTrue(testObject.joinGame());
+        assertTrue(testObject.connect());
     }
 
+    @Test
+    public void playerCanGetHand() {
+        Map<String, Integer> cards = testObject.getHand();
+        assertEquals(9, cards.values().stream().mapToInt(Integer::intValue).sum());
+    }
 }
